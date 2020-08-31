6edc7e2007baefefb4694a7b2b486df1174ba02e
Andrei Potecea <andreipotecea@yahoo.com>
2020-08-31 22:57:51 +0300

    final update here

--- src/main/java/com/ApiTheBest/ApiMicSiTare/model/appointmentModel/getAppointment/GetAppointment.java
+++ src/main/java/com/ApiTheBest/ApiMicSiTare/model/appointmentModel/getAppointment/GetAppointment.java
@@ -1,5 +1,64 @@
 package com.ApiTheBest.ApiMicSiTare.model.appointmentModel.getAppointment;
 
+import com.ApiTheBest.ApiMicSiTare.model.doctorModel.Doctor;
+import com.ApiTheBest.ApiMicSiTare.model.pacientModel.Pacient;
+import com.fasterxml.jackson.annotation.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "appointmentId",
+        "pacient",
+        "doctor",
+        "appointmentDate",
+        "appointmentTime"
+})
+
 public class GetAppointment {
 
+    @JsonProperty("appointmentId")
+    @JsonPropertyDescription(" appointmentId ")
+    private Integer appointmentId;
+
+    @JsonProperty("pacient")
+    @JsonPropertyDescription(" The pacient ")
+    @NotNull
+
+    private Pacient pacient;
+
+    @JsonProperty("doctor")
+    @JsonPropertyDescription(" The doctor")
+    private Doctor doctor;
+
+    @JsonProperty("appointmentDate")
+    @JsonPropertyDescription(" The appointmentDate.")
+    private LocalDate appointmentDate;
+
+    @JsonProperty("appointmentTime")
+    @JsonPropertyDescription(" The appointmentDate.")
+    private LocalTime appointmentTime;
+
+    @Override
+    public String toString() {
+        return "GetAppointment{" +
+                "appointmentId=" + appointmentId +
+                ", pacient=" + pacient +
+                ", doctor=" + doctor +
+                ", appointmentDate=" + appointmentDate +
+                ", appointmentTime=" + appointmentTime +
+                '}';
+    }
 }
