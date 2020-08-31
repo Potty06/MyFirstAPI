4d02897c71488c1cbe9e5d380ae3461508a1fd52
Andrei Potecea <andreipotecea@yahoo.com>
2020-08-31 22:44:32 +0300

    added updateAppointment

--- src/main/java/com/ApiTheBest/ApiMicSiTare/model/appointmentModel/updateAppointment/UpdateAppointment.java
+++ src/main/java/com/ApiTheBest/ApiMicSiTare/model/appointmentModel/updateAppointment/UpdateAppointment.java
@@ -1,4 +1,62 @@
 package com.ApiTheBest.ApiMicSiTare.model.appointmentModel.updateAppointment;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.ApiTheBest.ApiMicSiTare.model.doctorModel.Doctor;
+import com.ApiTheBest.ApiMicSiTare.model.pacientModel.Pacient;
+import com.fasterxml.jackson.annotation.*;
+import lombok.*;
+
+import javax.validation.constraints.*;
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "appointmentId",
+        "pacient",
+        "doctor",
+        "appointmentDate",
+        "appointmentDate"
+})
+
+
 public class UpdateAppointment {
+
+    @JsonProperty("pacient")
+    @JsonPropertyDescription(" The pacient ")
+    @NotNull
+
+    private Pacient pacient;
+
+    @JsonProperty("doctor")
+    @JsonPropertyDescription(" The doctor")
+    private Doctor doctor;
+
+    @JsonProperty("appointmentDate")
+    @JsonPropertyDescription(" The appointmentDate.")
+    private LocalDate appointmentDate;
+
+    @JsonProperty("appointmentTime")
+    @JsonPropertyDescription(" The appointmentDate.")
+    private LocalTime appointmentTime;
+
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @Override
+    public String toString() {
+        return "UpdatePacient{" +
+                "pacient=" + pacient +
+                ", doctor=" + doctor +
+                ", appointmentDate=" + appointmentDate +
+                ", appointmentTime=" + appointmentTime +
+                ", additionalProperties=" + additionalProperties +
+                '}';
+    }
 }
--- src/main/java/com/ApiTheBest/ApiMicSiTare/model/appointmentModel/updateAppointment/UpdateAppointmentRequest.java
+++ src/main/java/com/ApiTheBest/ApiMicSiTare/model/appointmentModel/updateAppointment/UpdateAppointmentRequest.java
@@ -1,4 +1,61 @@
 package com.ApiTheBest.ApiMicSiTare.model.appointmentModel.updateAppointment;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.ApiTheBest.ApiMicSiTare.model.pacientModel.updatePacient.UpdatePacient;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import javax.validation.Valid;
+
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "appointment"
+})
 public class UpdateAppointmentRequest {
+
+
+    @JsonProperty("appointment")
+    @JsonPropertyDescription(" the appointment updated into DB")
+    @Valid
+    private UpdatePacient appointment;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+
+    @JsonProperty("pacient")
+    public UpdatePacient getAppointment() {
+        return appointment;
+    }
+
+
+    @JsonProperty("appointment")
+    public void setAppointment(UpdatePacient appointment) {
+        this.appointment = appointment;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+    @Override
+    public String toString() {
+        return "UpdateAppointmentRequest{" +
+                "appointment=" + appointment +
+                ", additionalProperties=" + additionalProperties +
+                '}';
+    }
 }
