b79eb07c909c133aac192fc2e501cc93a9612541
Andrei Potecea <andreipotecea@yahoo.com>
2020-08-31 20:06:51 +0300

    My second commit

--- src/main/java/com/ApiTheBest/ApiMicSiTare/model/doctorModel/addDoctor/AddDoctorRequest.java
+++ src/main/java/com/ApiTheBest/ApiMicSiTare/model/doctorModel/addDoctor/AddDoctorRequest.java
@@ -1,4 +1,67 @@
 package com.ApiTheBest.ApiMicSiTare.model.doctorModel.addDoctor;
 
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import javax.validation.Valid;
+/**
+ * addCustomerReq
+ * <p>
+ * Add a new Customer to the DB.
+ *
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "doctor"
+})
+
 public class AddDoctorRequest {
+
+    /**
+     * the customer inserted into DB
+     *
+     */
+    @JsonProperty("doctor")
+    @JsonPropertyDescription(" the doctor inserted into DB")
+    @Valid
+    private AddDoctor doctor;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    /**
+     * the doctor inserted into DB
+     *
+     */
+    @JsonProperty("doctor")
+    public AddDoctor getDoctor() {
+        return doctor;
+    }
+
+    /**
+     * the doctor inserted into DB
+     *
+     */
+    @JsonProperty("doctor")
+    public void setDoctor(AddDoctor doctor) {
+        this.doctor = doctor;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+
 }
--- src/main/java/com/ApiTheBest/ApiMicSiTare/model/doctorModel/updateDoctor/UpdateDoctor.java
+++ src/main/java/com/ApiTheBest/ApiMicSiTare/model/doctorModel/updateDoctor/UpdateDoctor.java
@@ -1,4 +1,80 @@
 package com.ApiTheBest.ApiMicSiTare.model.doctorModel.updateDoctor;
 
+
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.*;
+
+import javax.validation.constraints.*;
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "doctorId",
+        "doctorName",
+        "phoneNo",
+        "contract",
+        "medicalSpeciality"
+})
+
 public class UpdateDoctor {
+
+    /**
+     * Name of the doctor.
+     */
+    @JsonProperty("doctorName")
+    @JsonPropertyDescription(" Name of the customer.")
+    @NotNull
+    @Size(min = 10, message = "Invalid name")
+    private String doctorName;
+    /**
+     * The phone number of the doctor.
+     */
+    @JsonProperty("phoneNo")
+    @JsonPropertyDescription(" The phone number of the doctor. ")
+    @Size(min = 9, max = 13, message = "Phone no should be from 2 and 34 digits maxim")
+    @Pattern(regexp = "(\\+40|0)[0-9]{9}")
+    private String phoneNo;
+
+    /**
+     * Type of contract.
+     */
+    @JsonProperty("contract")
+    @JsonPropertyDescription(" Type of contract")
+    @NotNull
+// sa nu uiti ma mamaie ma sa scrii pattern pt full time/part time
+    private String contract;
+    /**
+     * Medic Speciality.
+     */
+    @JsonProperty("medicSpeciality")
+    @JsonPropertyDescription(" Medic Speciality.")
+    @NotNull
+// validari de facut
+    private String medicSpeciality;
+
+
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @Override
+    public String toString() {
+        return "UpdateDoctor{" +
+                "doctorName='" + doctorName + '\'' +
+                ", phoneNo='" + phoneNo + '\'' +
+                ", contract='" + contract + '\'' +
+                ", medicSpeciality='" + medicSpeciality + '\'' +
+                ", additionalProperties=" + additionalProperties +
+                '}';
+    }
 }
